5b691158b795c1361183d5f4f15d5c67fbd957aa
yu-furukubo <138659773+yu-furukubo@users.noreply.github.com>
2023-08-09 07:39:58 +0000

    [start]kadai-3

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root to: "homes#top"
+  resources :books
+  resources :users, only: [:index, :show, :edit, :update]
+  get 'homes/about' => 'homes#about', as: 'about'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
